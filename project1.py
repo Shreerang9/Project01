255e95ff450546e8ee352e5303b667f102313dce
Shreerang9 <70016739+Shreerang9@users.noreply.github.com>
2020-08-24 14:58:31 +0530

    Update project1.py

--- project1.py
+++ project1.py
@@ -1,5 +1,5 @@
  #import pyttsx3
-import os
+import subprocess
 
 # pysttx3.speak("welcome to my tools", end='')
 
@@ -9,34 +9,41 @@ while True :
 	print(" chat with me with your requirements : " , end=' ' )
 	p=input()
 	# print(p)
-	# os.system(p)
+	# subprocess.getoutput(p)
 	
-	if (("run" in p) or ("open" in p)) and ("chrome" in p) and ("don't" not in p): 
-		os.system("chrome")
-	
+	if (("run" in p) or ("open" in p)) and ("chrome" in p) and ("don't" not in p):
+		subprocess.getoutput(" chrome ")
+
 	elif (("run" in p) or ("open" in p)) and ("whatsapp" in p) and ("don't" not in p):
-		os.system("chrome  web.whatsapp.com")
+		subprocess.getoutput("chrome  web.whatsapp.com")
+	
+	elif (("run" in p) or ("open" in p)) and ("youtube" in p) and ("don't" not in p):
+		subprocess.getoutput("chrome  www.youtube.com")
+
 
 	elif (("run" in p) or ("open" in p)) and ("google meet" in p) and ("don't" not in p):
-		os.system("chrome  meet.google.com")
+		subprocess.getoutput("chrome  meet.google.com")
 
 	elif (("run" in p) or ("open" in p)) and ("gmail" in p) and ("don't" not in p):
-	  	os.system("chrome  mail.google.com")
+		subprocess.getoutput("chrome  mail.google.com")
 
 	elif (("run" in p) or ("execute" in p)) and (("notepad" in p) or ("editor" in p)) and ("don't" not in p):
-		os.system("notepad")
+		subprocess.getoutput("notepad")
 
-	elif (("run" in p) or ("open" in p)) and ("zoom" in p) and ("don't" not in p) :
-		os.system("zoom")
+	elif (("run" in p) or ("open" in p)) and ("zoom" in p)and ("don't" not in p) :
+		subprocess.getoutput("zoom")
 
 	elif (("run" in p) or ("open" in p)) and ("vlc" in p) and ("don't" not in p) :
-		os.system("vlc")
+		subprocess.getoutput("vlc")
 	
-	elif (("run" in p) or ("open" in p)) and ("virtualbox" in p)  and ("don't" not in p) :
-		os.system("virtualbox")
+	elif (("run" in p) or ("open" in p)) and ("virtualbox" in p) and ("don't" not in p):
+		subprocess.getoutput("virtualbox")
 
 
-	elif  ("exit" in p)  or ("quit" in p) and ("don't" not in p) :
+	
+	
+
+	elif  ("exit" in p)  or ("quit" in p) and ("don't" not in p):
 		  break
 
 	else:
